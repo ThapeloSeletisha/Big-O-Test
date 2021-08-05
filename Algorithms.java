0df92e38dae389e616985de0244198d1ee65b9c9
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2021-08-05 15:49:13 +0200

    Implemented initialize function

--- Algorithms.java
+++ Algorithms.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 /**
  * This class defines functions
  * that implement algorithms
@@ -12,15 +13,27 @@ public class Algorithms {
         myTimer.stop();
         System.out.println("Time : " + myTimer.getTime());
     }
+    // Generates an array with random integers
+    // Returns the array
     public static int[] initialize(int size)
     {
-
+        int[] data = new int[size];
+        Random generator = new Random();
+        for (int i = 0; i < size; i++)
+        {
+            data[i] = generator.nextInt(1000000);
+        }
+        return data;
     }
+    // Counts the number of occurance of a some value
+    // returns that number
     public static int linearCount(int[] data, int value)
     {
-
+        
     }
-    public static int bubbleSort(int[] arr)
+    // Uses the bubble sort algorithm to sort the array
+    // Returns that array
+    public static int[] bubbleSort(int[] data)
     {
 
     }
