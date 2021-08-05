86ebab37d33ece51ccaab17bc9d805b94b605d3f
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2021-08-05 14:31:56 +0200

    Defined Timer class

---
+++ Timer.java
@@ -0,0 +1,28 @@
+// The class counts the amount of time passed
+// Used to assess the time an algorithm takes
+// args:
+//      none
+public class Timer {
+    
+    private long startTime;
+    private long endTime;
+
+    // Starts the timer
+    public void start()
+    {
+        
+    }
+
+    // Ends the timer
+    public void end()
+    {
+
+    }
+
+    // Returns the amount of time passed
+    // Returns -1 if an invalid call is made
+    public long getTime()
+    {
+        return -1;
+    }
+}
