9b67ce03b27510635b0f65851f6d2d5f30efa1fa
ThapeloSeletisha <thapeloseletisha01@gmail.com>
2021-08-05 16:03:52 +0200

    Implemented bubbleSort function

--- Algorithms.java
+++ Algorithms.java
@@ -43,7 +43,20 @@ public class Algorithms {
     // Returns that array
     public static int[] bubbleSort(int[] data)
     {
-        
+        int temp;
+        for (int i = 0; i < data.length; i++)
+        {
+            for (int j = i + 1; j < data.length; i++)
+            {
+                if (data[i] > data[j])
+                {
+                    temp = data[j];
+                    data[j] = data[i];
+                    data[i] = temp;
+                }
+            }
+        }
+        return data;
     }
     
 }
